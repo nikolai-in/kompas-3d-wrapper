5fd0c328cc859bde226cdbf193f53f69d548e291
Nick <nikolai.in@tuta.io>
2023-03-11 21:43:35 +0300

    🚧 Add some of kompas imports

--- src/kompas_3d_wrapper/__main__.py
+++ src/kompas_3d_wrapper/__main__.py
@@ -1,11 +1,51 @@
 """Command-line interface."""
+import sys
+from os import path
+
 import click
+import pythoncom  # noqa: F401
+
+
+KOMPAS_21_STUDY = r"C:\Program Files\ASCON\KOMPAS-3D v21 Study\Bin\kStudy.exe"
+KOMPAS_21_PYTHONWIN = (
+    r"C:\ProgramData\ASCON\KOMPAS-3D\21\Python 3\App\Lib\site-packages\pythonwin"
+)
+
+
+def import_kompas_ldefin2d_and_mischelpers(kompas_pythonwin: str) -> tuple:
+    """Import Kompas LDefin2D and miscHelpers modules."""
+    # Check if the kompas_pythonwin path exists, otherwise raise FileNotFoundError
+    if not path.exists(kompas_pythonwin):
+        raise FileNotFoundError(
+            "Kompas pythonwin not found. Please install Kompas with macro support."
+        ) from None
+
+    sys.path.append(kompas_pythonwin)
+
+    try:
+        # Import LDefin2D and MiscellaneousHelpers as miscHelpers modules
+        import LDefin2D  # pyright: reportMissingImports=false
+        import MiscellaneousHelpers as miscHelpers
+    except ImportError:
+        # If either of the modules above cannot be imported, raise the same ImportError
+        raise ImportError(
+            "Kompas LDFine2D and miscHelpers modules not found."
+        ) from ImportError
+    finally:
+        # Ensure that the sys.path is restored
+        sys.path.remove(kompas_pythonwin)
+
+    # Return the imported modules as a tuple after the try-finally block has completed
+    return LDefin2D, miscHelpers
 
 
 @click.command()
 @click.version_option()
 def main() -> None:
     """Kompas 3D Wrapper."""
+    ldefin2d, misc_helpers = import_kompas_ldefin2d_and_mischelpers(KOMPAS_21_PYTHONWIN)
+    print(type(ldefin2d))
+    print(type(misc_helpers))
 
 
 if __name__ == "__main__":
