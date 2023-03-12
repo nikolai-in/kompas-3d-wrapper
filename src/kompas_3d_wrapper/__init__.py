0ff183e6dc756ae4d64b6e8fa369d8eab7bdf005
Nick <nikolai.in@tuta.io>
2023-03-12 20:24:23 +0300

    Refactor the import statement for starting KOMPAS 3D to use the updated function name

--- src/kompas_3d_wrapper/__init__.py
+++ src/kompas_3d_wrapper/__init__.py
@@ -3,12 +3,12 @@
 from kompas_3d_wrapper.__main__ import get_kompas_api5
 from kompas_3d_wrapper.__main__ import get_kompas_api7
 from kompas_3d_wrapper.__main__ import import_kompas_ldefin2d_mischelpers
-from kompas_3d_wrapper.__main__ import start_kompas_if_not_running
+from kompas_3d_wrapper.__main__ import start_kompas
 
 
 __all__ = [
     "import_kompas_ldefin2d_mischelpers",
-    "start_kompas_if_not_running",
+    "start_kompas",
     "get_kompas_api5",
     "get_kompas_api7",
 ]
