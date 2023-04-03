c74b848cd4cee100400c0011afb89a7d36b38daa
Nick <nikolai.in@tuta.io>
2023-04-03 04:18:19 +0300

    🚧 Move main into __main__ xD

--- src/kompas_3d_wrapper/__init__.py
+++ src/kompas_3d_wrapper/__init__.py
@@ -1,21 +1,7 @@
 """Kompas 3D Wrapper."""
-from kompas_3d_wrapper.main import find_exe_by_file_extension
-from kompas_3d_wrapper.main import get_kompas_api5
-from kompas_3d_wrapper.main import get_kompas_api7
-from kompas_3d_wrapper.main import get_kompas_constants
-from kompas_3d_wrapper.main import get_kompas_path
-from kompas_3d_wrapper.main import get_pythonwin_path
-from kompas_3d_wrapper.main import import_ldefin2d_mischelpers
-from kompas_3d_wrapper.main import start_kompas
+from kompas_3d_wrapper.__main__ import Kompas
+from kompas_3d_wrapper.__main__ import find_exe_by_file_extension
+from kompas_3d_wrapper.__main__ import is_process_running
 
 
-__all__ = [
-    "find_exe_by_file_extension",
-    "get_kompas_api5",
-    "get_kompas_api7",
-    "get_kompas_constants",
-    "get_kompas_path",
-    "get_pythonwin_path",
-    "import_ldefin2d_mischelpers",
-    "start_kompas",
-]
+__all__ = ["find_exe_by_file_extension", "is_process_running", "Kompas"]
